104925199254054e92ae33020c4a52e4c85509ee
ismail <icb_1998@hotmail.com>
2023-12-01 02:31:43 +0300

    phone adress service

---
+++ IsbaSatisBlazor/Server/Services/Infrastruce/IAdressService.cs
@@ -0,0 +1,18 @@
+﻿using IsbaSatisBlazor.Shared.DTO;
+
+namespace IsbaSatisBlazor.Server.Services.Infrastruce
+{
+    public interface IAdressService
+    {
+        public Task<AdressDTO> GetAdressById(Guid Id);
+
+        public Task<List<AdressDTO>> GetAdresss();
+        public Task<List<AdressDTO>> GetAdresssById(Guid Id);
+
+        public Task<AdressDTO> CreateAdress(AdressDTO Adress);
+
+        public Task<AdressDTO> UpdateAdress(AdressDTO Adress);
+
+        public Task<bool> DeleteAdressId(Guid Id);
+    }
+}
